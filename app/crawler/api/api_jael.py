51ebef6d6862d05b16d8607c84c3a9d210902553
michaelnv <michaelnv@mail.ru>
2019-05-17 09:05:07 +0500

    Improved API data import

--- app/crawler/api/api_jael.py
+++ app/crawler/api/api_jael.py
@@ -17,16 +17,19 @@ class ApiJael(ApiBase):
         r = requests.get(self.get_url(), params=params)
         hotels = r.json()
 
+        imported_rows = 0
         if hotels:
             from ..models import Hotel
 
             for hotel in hotels:
-                Hotel.objects.create(
-                    hotel=hotel['name'],
-                    country=hotel['country'],
-                    phone=hotel['phone'],
-                    source=self.get_url(),
-                    raw_data=hotel,
-                )
+                if not Hotel.objects.filter(hotel=hotel['name'], country=hotel['country']).exists():
+                    Hotel.objects.create(
+                        hotel=hotel['name'],
+                        country=hotel['country'],
+                        phone=hotel['phone'],
+                        source=self.get_url(),
+                        raw_data=hotel,
+                    )
+                    imported_rows += 1
 
-        return len(hotels)
+        return imported_rows
